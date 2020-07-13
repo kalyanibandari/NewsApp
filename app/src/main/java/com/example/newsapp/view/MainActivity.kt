4740680ecd4e2f425c5a8def11806572d7f73e3b
kalyanibandari <kalyanibandari1995@gmail.com>
2020-07-13 21:22:37 +0530

    Added ads in news feed, added progress bar in MainActivity and WebviewActivity

--- app/src/main/java/com/example/newsapp/view/MainActivity.kt
+++ app/src/main/java/com/example/newsapp/view/MainActivity.kt
@@ -1,22 +1,30 @@
 package com.example.newsapp.view
 
+import android.Manifest
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
-import android.webkit.WebView
+import android.view.View
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.newsapp.R
 import com.example.newsapp.adapter.NewsAdapter
 import com.example.newsapp.model.News
 import com.example.newsapp.service.RetrofitInstance
+import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.AdView
+import com.google.android.gms.ads.MobileAds
 import kotlinx.android.synthetic.main.activity_main.*
 import retrofit2.Call
-import retrofit2.Response
 import retrofit2.Callback
+import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var mAdView : AdView
     lateinit var adapter: NewsAdapter
     lateinit var layoutManager : LinearLayoutManager
 
@@ -24,6 +32,31 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        MobileAds.initialize(this)
+        mAdView = findViewById(R.id.adView)
+        val adRequest = AdRequest.Builder().build()
+        mAdView.loadAd(adRequest)
+
+        checkPermission()
+    }
+
+    fun checkPermission(){
+        if (Build.VERSION.SDK_INT >= 23) {
+            if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED &&
+                checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                displayNews()
+            } else {
+                ActivityCompat.requestPermissions(
+                    this, arrayOf(
+                        Manifest.permission.ACCESS_FINE_LOCATION,
+                        Manifest.permission.ACCESS_COARSE_LOCATION
+                    ), 1
+                )
+            }
+        }
+    }
+
+    fun displayNews(){
         newsRecyclerView.setHasFixedSize(true)
         layoutManager = LinearLayoutManager(this)
         newsRecyclerView.layoutManager = layoutManager
@@ -35,27 +68,29 @@ class MainActivity : AppCompatActivity() {
         requestCall.enqueue(object : Callback<News>{
 
             override fun onResponse(call: Call<News>, response: Response<News>) {
-
                 adapter = NewsAdapter(this@MainActivity, response.body()?.articles){articles ->
-                    Toast.makeText(this@MainActivity, "clicked", Toast.LENGTH_SHORT).show()
+                  //  Toast.makeText(this@MainActivity, "clicked", Toast.LENGTH_SHORT).show()
                     val webViewIntent = Intent(this@MainActivity, WebViewActivity::class.java)
                     webViewIntent.putExtra("url", articles.url)
                     startActivity(webViewIntent)
-
                 }
                 adapter.notifyDataSetChanged()
+                progressbar.visibility = View.GONE
                 newsRecyclerView.adapter = adapter
-
-                Toast.makeText(this@MainActivity, "success", Toast.LENGTH_SHORT).show()
-                //status_text.text = response.body()?.status
-
             }
 
             override fun onFailure(call: Call<News>, t: Throwable) {
                 Toast.makeText(this@MainActivity, "failed", Toast.LENGTH_SHORT).show()
             }
-
         })
+    }
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        if (requestCode == 1 && grantResults[0] == PackageManager.PERMISSION_GRANTED && grantResults[1] == PackageManager.PERMISSION_GRANTED
+        ) {
+            displayNews()
+        } else {
+            checkPermission()
+        }
     }
 }
 
--- app/src/main/java/com/example/newsapp/view/WebViewActivity.kt
+++ app/src/main/java/com/example/newsapp/view/WebViewActivity.kt
@@ -1,18 +1,36 @@
 package com.example.newsapp.view
 
+import android.graphics.Bitmap
 import android.os.Bundle
+import android.view.View
 import android.webkit.WebView
+import android.webkit.WebViewClient
+import android.widget.ProgressBar
 import androidx.appcompat.app.AppCompatActivity
 import com.example.newsapp.R
-import kotlinx.android.synthetic.main.activity_web_view.*
+
 
 class WebViewActivity : AppCompatActivity() {
 
+    var progressBar: ProgressBar? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val webView = WebView(this)
-        setContentView(webView)
+        setContentView(R.layout.activity_web_view)
+        val myWebView = findViewById<WebView>(R.id.webView)
+        progressBar = findViewById(R.id.webViewProgressBar)
         val url = intent.getStringExtra("url")
-        webView.loadUrl(url)
+        myWebView.settings.javaScriptEnabled = true
+        myWebView.webViewClient = WebViewClient()
+        myWebView.setWebViewClient(myWebClient())
+        myWebView.loadUrl(url)
+    }
+
+    inner class myWebClient : WebViewClient() {
+
+        override fun onPageStarted(view: WebView?, url: String?, favicon: Bitmap?) {
+            super.onPageStarted(view, url, favicon)
+            progressBar?.visibility = View.GONE
+        }
     }
 }
